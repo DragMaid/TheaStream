0f64c65463f538474e5591678138d142ed143b70
VuHoangHaiBinh <vhoanghaibinh@gmail.com>
2025-06-12 16:06:58 +0800

    update asgi application for livestream

--- TheaStream/asgi.py
+++ TheaStream/asgi.py
@@ -1,16 +1,16 @@
-"""
-ASGI config for TheaStream project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import livestream.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TheaStream.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            livestream.routing.websocket_urlpatterns
+        )
+    ),
+})
